45d6812b5f20a9a1ba5c25ccf668ab00523f4052
исп-22 <-22@LAB30-04>
2024-12-06 15:08:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти 100 первых простых чисел.
+
+
+int C = 0;
+for (int a = 2; C <= 100; a++)
+{
+    bool CH = true;
+    for (int b = 2; b <= Math.Sqrt(a); b++)
+    {
+        if (a % b == 0) CH = false;
+    }
+    if (CH)
+    {
+        Console.Write(a + " ");
+        C++;
+    }
+}
+Console.ReadKey();
